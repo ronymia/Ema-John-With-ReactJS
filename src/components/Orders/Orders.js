d7e5c9dd164a75a5090cf5ef2845cb079348e943
MD Rony Mia <official.mdronymia@gmail.com>
2022-03-31 19:58:55 +0600

    useCart hook added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
+
     return (
         <div>
             <h1>Total items {products.length}</h1>
+            <h3>cart length : {cart.length}</h3>
         </div>
     );
 };
