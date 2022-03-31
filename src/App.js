71ea7060ba537ebfafc940a177c72f0996794a0d
MD Rony Mia <official.mdronymia@gmail.com>
2022-03-31 18:37:29 +0600

    React Router install

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Header from './components/Header/Header'
 import Shop from './components/Shop/Shop';
+import {
+  Routes,
+  Route
+} from 'react-router-dom';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
 
 function App() {
   return (
     <div >
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path='/' element={<Shop></Shop>}></Route>
+        <Route path='/home' element={<Shop></Shop>}></Route>
+        <Route path="/orders" element={<Orders />}></Route>
+        <Route path="/inventory" element={<Inventory />}></Route>
+      </Routes>
     </div>
   );
 }
