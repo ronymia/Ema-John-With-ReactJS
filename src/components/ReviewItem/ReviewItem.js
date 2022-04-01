9da547661997fae9c2229e31573ccf16fff56924
MD Rony Mia <official.mdronymia@gmail.com>
2022-04-01 15:47:18 +0600

    ReviewItems

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -7,7 +7,20 @@ const ReviewItem = (props) => {
 
     return (
         <div className='reviewItem'>
-            <h3>{name}</h3>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className="reviewitem-details-container">
+                <div className="reviewitem-details">
+                    <p><span className="item-name">{name}</span></p>
+                    <p>Price : <span className="orange-color">{price}</span></p>
+                    <p>Shipping Charge : <span className="orange-color">{shipping}</span></p>
+                    <p>Quantity : <span className="orange-color">{quantity}</span></p>
+                </div>
+                <div className="delete-item">
+                    <button>delete</button>
+                </div>
+            </div>
         </div>
     );
 };
