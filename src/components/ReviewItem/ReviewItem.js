ccd422ccee3b0901d78551bfd953cbb038ab6397
MD Rony Mia <official.mdronymia@gmail.com>
2022-04-01 16:17:20 +0600

    Review Order style Complete

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,3 +1,5 @@
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ReviewItms.css';
 
@@ -13,12 +15,14 @@ const ReviewItem = (props) => {
             <div className="reviewitem-details-container">
                 <div className="reviewitem-details">
                     <p><span className="item-name">{name}</span></p>
-                    <p>Price : <span className="orange-color">{price}</span></p>
-                    <p>Shipping Charge : <span className="orange-color">{shipping}</span></p>
-                    <p>Quantity : <span className="orange-color">{quantity}</span></p>
+                    <p>Price : <small className="orange-color">${price}</small></p>
+                    <p>Shipping Charge : <small className="orange-color">${shipping}</small></p>
+                    <p>Quantity : <small className="orange-color">{quantity}</small></p>
                 </div>
                 <div className="delete-item">
-                    <button>delete</button>
+                    <button className='delete-btn'>
+                        <FontAwesomeIcon className='delete-icon' icon={faTrashAlt}></FontAwesomeIcon>
+                    </button>
                 </div>
             </div>
         </div>
